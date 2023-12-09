b1cc0b0efda5287f8634bc87c1dea703df6d0cae
Simon <sooguan@gmail.com>
2023-12-09 22:38:27 +0800

    Add files via upload

---
+++ ListFiles_LastModified.ps1
@@ -0,0 +1,17 @@
+﻿$targetfolder = 'C:\Users\esg\Documents\Virtual Machines'
+$outputFile = 'C:\users\esg\Desktop\simon2.csv'
+$dataColl = Get-ChildItem -Recurs -Force $targetfolder -ErrorAction SilentlyContinue | ForEach-Object {
+   $len = Get-ChildItem -Recurse -Force $_.FullName -File -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum | Select-Object -ExpandProperty Sum
+   $filelastaccess = $_.LastAccessTime.ToString("yyyy-MM-dd HH:mm:ss")
+   $filename = $_.FullName
+   $filesize = '{0:F2}' -f ($len/1024)
+   [PSCustomObject]@{
+       sizeKB = $filesize
+       name = $filename
+       lastmodified = $filelastaccess
+
+   }
+}
+$dataColl | Export-Csv -Path $outputFile -NoTypeInformation -Delimiter ‘;’
+#set-content $outputFile ((get-content $outputFile) -replace '"')
+

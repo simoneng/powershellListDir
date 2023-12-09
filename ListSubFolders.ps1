f8f3cacaea773355986728839bf4228358c6de35
Simon <sooguan@gmail.com>
2023-12-09 21:13:55 +0800

    Update ListSubFolders.ps1

--- ListSubFolders.ps1
+++ ListSubFolders.ps1
@@ -3,7 +3,7 @@ $outputFile = 'C:\temp\output.csv'
 $dataColl = Get-ChildItem -Force $targetfolder -Directory -ErrorAction SilentlyContinue | ForEach-Object {
    $len = Get-ChildItem -Recurse -Force $_.FullName -File -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum | Select-Object -ExpandProperty Sum
    $foldername = $_.FullName
-   $foldersize = '{0:N2}' -f ($len/1024)
+   $foldersize = '{0:F2}' -f ($len/1024)
    [PSCustomObject]@{
        foldersizeKB = $foldersize
        foldername = $foldername
